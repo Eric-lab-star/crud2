6086e93118ddefbaec154ea4389064d3bc523309
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-09-01 11:08:53 +0900

    connecting to db

--- src/db.js
+++ src/db.js
@@ -1,3 +1,8 @@
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://127.0.0.1:27017/recap1");
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.log("DB error", error));
+db.once("open", () => console.log(" ✅ Connected to DB"));
