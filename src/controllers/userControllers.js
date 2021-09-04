9599cc97236c78e26b40d7a5066c51e87bfcc6f4
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-09-04 18:43:51 +0900

    join form

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,3 +1,12 @@
+import User from "../models/User";
+
 export const login = (req, res) => {
   res.render("Login", { pageTitle: "Login" });
+};
+
+export const getJoin = (req, res) => {
+  return res.render("join", { pageTitle: "Create Account" });
+};
+export const postJoin = (req, res) => {
+  return res.redirect("/");
 };
--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import "./db";
 import "./models/Video";
+import "./models/User";
 import server from "./server";
 server.listen(4000, () => console.log("listening to http://localhost:4000"));
