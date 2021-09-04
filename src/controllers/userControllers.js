172b8e4d13c78ee8b8d6313a084035d4708b6552
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-09-04 23:33:27 +0900

    and form vaildation

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -8,8 +8,24 @@ export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Create Account" });
 };
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
-  const users = await User.create({
+  const { name, username, email, password, password2, location } = req.body;
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "Password confirmation does not match",
+    });
+  }
+  const joinVaildation = await User.exists({
+    $or: [{ email, username, name }],
+  });
+  if (joinVaildation) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "This account is already taken",
+    });
+  }
+
+  await User.create({
     name,
     username,
     email,
