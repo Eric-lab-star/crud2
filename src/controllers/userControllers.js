98e5c9d7825025360eb47abaab2f2e5398e3ad02
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-09-04 23:00:43 +0900

    password hashing

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -7,6 +7,14 @@ export const login = (req, res) => {
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Create Account" });
 };
-export const postJoin = (req, res) => {
-  return res.redirect("/");
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  const users = await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/users/login");
 };
