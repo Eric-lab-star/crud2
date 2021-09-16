706ee05f5df92d52d077eb3e96d165b0b0c916f9
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-09-16 20:20:29 +0900

    user login and join

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,1 +1,62 @@
-export const user = (req, res) => res.send("user");
+import User from "../models/User";
+import bcrypt from "bcrypt";
+
+export const user = (req, res) => {
+  return res.send("user");
+};
+
+export const getJoin = (req, res) => {
+  return res.render("join", { pageTitel: "Join" });
+};
+
+export const postJoin = async (req, res) => {
+  const { username, name, password, location, email, password2 } = req.body;
+  const exist = await User.exists({ username });
+  if (exist) {
+    return res.render("login", {
+      errorMessage: " This username already exists",
+    });
+  }
+  if (password !== password2) {
+    return res.render("login", { errorMessage: "Password Validation Fail" });
+  }
+  try {
+    await User.create({
+      username,
+      name,
+      password,
+      location,
+      email,
+      password,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.redirect("/join", { errorMessage: error._message });
+  }
+};
+
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "Login" });
+};
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  const exist = await User.exists({ username });
+
+  if (!exist) {
+    return res.render("login", {
+      pageTitle: "Login",
+      errorMessage: "This username does not exists",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.render("Login", {
+      pageTitle: "Login",
+      errorMessage: "Password confirmation failed",
+    });
+  }
+
+  return res.redirect("/");
+};
