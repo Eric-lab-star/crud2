d393c616641646908840ac6cada27ed83669a0ce
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-10-19 14:22:33 +0900

    aws is grete

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -24,8 +24,8 @@ export const postUpload = async (req, res) => {
     const newVideo = await Video.create({
       owner: _id,
       title,
-      fileUrl: video[0].path,
-      thumbUrl: thumb[0].path,
+      fileUrl: video[0].location,
+      thumbUrl: thumb[0].location,
       description,
       hashtag: hashtag
         .split(",")
